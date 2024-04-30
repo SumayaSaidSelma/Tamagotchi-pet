917cf030d1b1f38fa48283a2bd6969ff95e1040e
SumayaSaidSelma <sumayasaidselma@gmail.com>
2024-04-29 22:06:10 -0400

    updated js

--- script.js
+++ script.js
@@ -19,18 +19,21 @@ function updateMetrics() {
     document.getElementById("age").textContent = pet.age;
 }
 
-function feed() {
+function feedPet(){
     if (pet.hunger < 10) {
         pet.hunger++;
+        pet.boredom--;
         updateMetrics();
-        document.getElementById("pet-img").classList.add("eating");
-        setTimeout(() => {
-            document.getElementById("pet-img").classList.remove("eating");
-        }, 500);
+        document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYng1eWthbHI1YTFmMHMzdzkxaHp4d2Ryc2N5ZXE0cjlldjhraXVkdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/WLohQRn2Iqp9WOBPT2/giphy.gif"); // Change pet image to eating
+        if (pet.hunger >= 8) {
+            setTimeout(() => {
+                document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/zBjObo9umi0wbrCRdP/giphy.gif"); // Change pet image to dancing after eating
+            }, 2000); // Change back to dancing after 2 seconds
+        }
     } else {
         pet.hunger = 0; // Reset hunger
         updateMetrics();
-        document.getElementById("pet-img").setAttribute("src", "pictures/im-so-tired-stuart.gif");
+        document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/zBjObo9umi0wbrCRdP/giphy.gif"); // change image to dancing 
         document.getElementById("pet-img").classList.add("crying");
         setTimeout(() => {
             document.getElementById("pet-img").classList.remove("crying");
@@ -38,37 +41,55 @@ function feed() {
     }
 }
 
-function turnOffLights() {
-    if (pet.sleepiness > 1) {
-        pet.sleepiness--;
-        updateMetrics();
-        document.getElementById("pet-img").classList.add("waking-up");
-        setTimeout(() => {
-            document.getElementById("pet-img").classList.remove("waking-up");
-        }, 1000);
-        document.querySelector('.background').style.backgroundImage = "pictures/night-space.png";
-    }
-}
-
 
 function play() {
-    if (pet.boredom > 1) {
-        pet.boredom--;
+    if (pet.boredom < 10) {
+        pet.boredom++;
+        pet.hunger++;
         updateMetrics();
+        document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/kKdXxIa4QdKzC/giphy.gif");
+        if (pet.boredom === 8) {
+            document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/4Z3agcuCNFBeI/giphy.gif");
+        }
+    } else {
+        pet.boredom = 0; //reset
+        updateMetrics();
+        document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/zBjObo9umi0wbrCRdP/giphy.gif"); // change image to dancing 
+        document.getElementById("pet-img").classList.add("crying");
+        setTimeout(() => {
+            document.getElementById("pet-img").classList.remove("crying");
+        }, 500);
     }
 }
 
-
 function sleep() {
-    if (pet.sleepiness < 10) {
-        pet.sleepiness++;
+    if (pet.sleepiness > 0) {
+        pet.sleepiness--;
+        pet.hunger++;
+        pet.boredom++;
         updateMetrics();
-        document.getElementById("pet-img").classList.add("sleeping");
+        if (pet.sleepiness === 2) {
+            document.querySelector('.background').style.backgroundImage = "url('pictures/night-space.png')";
+            document.getElementById("pet-img").setAttribute("src", "pictures/im-so-tired.gif"); // Change pet image
+        }
+    } else if (pet.sleepiness < 0){
+        pet.sleepiness = 5; // Reset sleepiness to a positive value
+        updateMetrics();
+        document.querySelector('.background').style.backgroundImage = "url('pictures/space-background.jpg')";
+        document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/zBjObo9umi0wbrCRdP/giphy.gif"); // change image to dancing 
+        document.getElementById("pet-img").classList.add("crying");
         setTimeout(() => {
-            document.getElementById("pet-img").classList.remove("sleeping");
-        }, 1000);
-        document.querySelector('.background').style.backgroundImage = "pictures/night-space.png";
-        document.getElementById("pet-img").setAttribute("src", "pictures/im-so-tired.gif"); // Change pet image
+            document.getElementById("pet-img").classList.remove("crying");
+        }, 500);
+    } else {
+        pet.sleepiness = -10; // Reset sleepiness to a positive value
+        updateMetrics();
+        document.querySelector('.background').style.backgroundImage = "url('pictures/space-background.jpg')";
+        document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/zBjObo9umi0wbrCRdP/giphy.gif"); // change image to dancing 
+        document.getElementById("pet-img").classList.add("crying");
+        setTimeout(() => {
+            document.getElementById("pet-img").classList.remove("crying");
+        }, 500);
     }
 }
 
@@ -80,7 +101,7 @@ function increaseAge() {
 }
 
 function increaseMetrics() {
-    if (pet.hunger > 1) {
+    if (pet.hunger < 10) {
         pet.hunger--;
     }
     if (pet.sleepiness < 10) {
@@ -92,16 +113,35 @@ function increaseMetrics() {
     updateMetrics();
 }
 
+function resetGame() {
+    pet.boredom = 5;
+    pet.age = 0;
+    pet.hunger = 5;
+    pet.sleepiness = 5;
+    updateMetrics();
+}
+
 function checkStatus() {
-    if (pet.hunger === 10 || pet.sleepiness === 10 || pet.boredom === 10) {
+    if (pet.hunger === 0 || pet.boredom === 0 || pet.sleepiness === 0) {
         alert("Oh no! Your pet has died!");
         clearInterval(interval);
     }
 }
+
+
+function win() {
+    if (pet.hunger === 10 || pet.boredom === 10 || pet.sleepiness === 10) {
+        alert("Oh YESS! Your pet is winning!");
+        clearInterval(interval);
+            setTimeout(() => {
+                document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/XlGQ2sBz4gvIAv0FVa/giphy.gif"); // Change pet image to dancing after eating
+            }, 2000); // Change back to dancing after 2 seconds
+    }
+}
+
 
 let interval = setInterval(() => {
     increaseAge();
     increaseMetrics();
     checkStatus();
 }, 5000);
-
