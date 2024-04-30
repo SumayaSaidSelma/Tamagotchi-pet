95337c762e503760463be336db148d53a92221df
SumayaSaidSelma <sumayasaidselma@gmail.com>
2024-04-29 23:18:58 -0400

    update js

--- script.js
+++ script.js
@@ -25,10 +25,8 @@ function feedPet(){
         pet.boredom--;
         updateMetrics();
         document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYng1eWthbHI1YTFmMHMzdzkxaHp4d2Ryc2N5ZXE0cjlldjhraXVkdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/WLohQRn2Iqp9WOBPT2/giphy.gif"); // Change pet image to dancing
-        if (pet.hunger >= 8) {
-            setTimeout(() => {
-                document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/zBjObo9umi0wbrCRdP/giphy.gif"); // Change pet image to hungry after need to eat
-            }, 2000); // Change back to dancing after 2 seconds
+        if (pet.hunger === 9) {
+                document.getElementById("pet-img").setAttribute("src", "pictures/huging-banana.png"); // Change pet image to hungry after need to eat
         }
     } else {
         pet.hunger = 0; // Reset hunger
@@ -44,7 +42,7 @@ function play() {
         pet.sleepiness++;
         updateMetrics();
         document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYng1eWthbHI1YTFmMHMzdzkxaHp4d2Ryc2N5ZXE0cjlldjhraXVkdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/WLohQRn2Iqp9WOBPT2/giphy.gif"); // chnage image to dancing 
-        if (pet.boredom >= 8) {
+        if (pet.boredom === 8) {
             document.getElementById("pet-img").setAttribute("src", "pictures/im-so-tired-stuart.gif"); // chnage image to tired 
         }
     } else {
@@ -59,18 +57,15 @@ function sleep() {
         pet.hunger--;
         pet.boredom++;
         updateMetrics();
-        if (pet.sleepiness <= 2) {
-            document.querySelector('.background').style.backgroundImage = "url('https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExejR2cHY3aHFoenhvNGxqM3dpbnFzaXc5eWRoNnIyNzNnczhzdGU0MiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/FMsbSSjEe0N6zH7K7x/giphy-downsized-large.gif')";
+        document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYng1eWthbHI1YTFmMHMzdzkxaHp4d2Ryc2N5ZXE0cjlldjhraXVkdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/WLohQRn2Iqp9WOBPT2/giphy.gif"); // chnage image to dancing 
+        document.querySelector('.background').style.backgroundImage = "url('pictures/gjwF3CaJSee2Hr3LUodYyJ-1200-80.jpg')";
+        if (pet.sleepiness === 10) {
+            document.querySelector('.background').style.backgroundImage = "url('https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNndmdHd2NjltdDA0aXg5d3ZpNmY0bjBtYTl4dnl2ajA5djE1MWdnNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l1Et7Qw1XjvunS0Io/giphy.gif')";
             document.getElementById("pet-img").setAttribute("src", "pictures/minion-sleeping.png"); // change image to sleeping 
-            document.getElementById("pet-img").classList.add("crying");
-            setTimeout(() => {
-                document.getElementById("pet-img").classList.remove("crying");
-            }, 500);
         }
         } else {
         pet.sleepiness = 5; // Reset sleepiness to a positive value
         updateMetrics();
-            document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYng1eWthbHI1YTFmMHMzdzkxaHp4d2Ryc2N5ZXE0cjlldjhraXVkdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/WLohQRn2Iqp9WOBPT2/giphy.gif"); // chnage image to dancing 
     }
 }
 
@@ -100,6 +95,8 @@ function resetGame() {
     pet.hunger = 5;
     pet.sleepiness = 5;
     updateMetrics();
+    document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYng1eWthbHI1YTFmMHMzdzkxaHp4d2Ryc2N5ZXE0cjlldjhraXVkdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/WLohQRn2Iqp9WOBPT2/giphy.gif"); // chnage image to dancing 
+    document.querySelector('.background').style.backgroundImage = "url('pictures/gjwF3CaJSee2Hr3LUodYyJ-1200-80.jpg')";
 }
 
 function checkStatus() {
@@ -115,7 +112,7 @@ function win() {
         alert("Oh YESS! Your pet is winning!");
         clearInterval(interval);
             setTimeout(() => {
-                document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/XlGQ2sBz4gvIAv0FVa/giphy.gif"); // Change pet image to dancing after eating
+                document.getElementById("pet-img").setAttribute("src", "https://media.giphy.com/media/XlGQ2sBz4gvIAv0FVa/giphy.gif"); // Change pet image to circle wining image 
             }, 2000); // Change back to dancing after 2 seconds
     }
 }
